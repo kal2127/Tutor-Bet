e58dafbdd6974e1ddb872c872d2c208b42e0f2b0
kal2127 <27kalkidan21@gmail.com>
2026-09-19 18:27:55 +0300

    edit uploadReciept.js code duplication issue

--- tutor-platform-server/src/middleware/uploadReciept.js
+++ tutor-platform-server/src/middleware/uploadReciept.js
@@ -18,24 +18,4 @@ module.exports = multer({
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB
   },
-});const multer = require("multer");
-
-// Use memory storage so it doesn't try to write files to a local hard drive
-const storage = multer.memoryStorage();
-
-// Allow only images
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image files are allowed"));
-  }
-};
-
-module.exports = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
 });
